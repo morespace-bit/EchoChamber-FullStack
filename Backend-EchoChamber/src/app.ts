365e243198b39b48f62badee07f8baa97c6cdce7
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2025-07-01 20:09:33 +0545

    added cors cross origin resources sharing:

--- Backend-EchoChamber/src/app.ts
+++ Backend-EchoChamber/src/app.ts
@@ -4,6 +4,7 @@ const app = express();
 import authRoute from "./routes/auth.route";
 import profileRoute from "./routes/profile.route";
 import postRoute from "./routes/post.route";
+import cors from "cors";
 
 // this is done so that our express app can parse the incomming data that comes in the json format
 app.use(express.json());
@@ -11,6 +12,12 @@ app.use(express.json());
 // app.use("/home", (req: Request, res: Response) => {
 //   res.send("Hello");
 // });
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use("/api", authRoute);
 app.use("/api", profileRoute);
 app.use("/api", postRoute);
