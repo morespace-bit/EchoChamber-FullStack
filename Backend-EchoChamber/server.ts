e250af73f6ca416390857697099b13548c2882d7
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2025-07-25 16:35:39 +0545

    fixing

--- Backend-EchoChamber/server.ts
+++ Backend-EchoChamber/server.ts
@@ -3,10 +3,4 @@ import "./src/database/connection";
 
 import app from "./src/app";
 
-const serverRun = () => {
-  app.listen(dotEnv.port, () => {
-    console.log(`The server is running on the port ${dotEnv.port}`);
-  });
-};
-
-serverRun();
+export default app;
