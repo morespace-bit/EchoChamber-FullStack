6130970bcbefa0bdfe2b9d89fe99cc0479f64502
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2025-07-25 16:43:00 +0545

    debugging

--- Backend-EchoChamber/server.ts
+++ Backend-EchoChamber/server.ts
@@ -1,6 +1,13 @@
 import { dotEnv } from "./src/config/config";
 import "./src/database/connection";
+import express from "express";
 
-import app from "./src/app";
+// import app from "./src/app";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("✅ Server is working!");
+});
 
 export default app;
